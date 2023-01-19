55d17b7986bc95b8b90ffb9b2836da0ba08dbef2
Dary1Tang <daryltang2002@gmail.com>
2023-01-19 15:42:04 +0800

    Completed the js file and ready to ne played

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let rps = ['rock','paper','scissors']
 
-let computerSelection = getComputerChoice();
-let playerSelection = getPlayerChoice();
-//console.log(computerSelection);
-playRound(computerSelection,playerSelection);
+//let computerSelection = getComputerChoice();
+//let playerSelection = getPlayerChoice();
+
+game();
 
 function playRound(computerSelection,playerSelection) {
     console.log('player choice:' + playerSelection);
@@ -11,14 +11,17 @@ function playRound(computerSelection,playerSelection) {
 
     if(computerSelection == playerSelection){
         console.log('Tie');
+        return 'tie';
     } else if(
     (computerSelection == 'rock' && playerSelection == 'scissors') ||
     (computerSelection == 'paper' && playerSelection == 'rock') ||
     (computerSelection == 'scissors' && playerSelection == 'paper')
     ){
         console.log('Computer wins!');
+        return 'computer';
     } else {
         console.log('Player wins!');
+        return 'player';
     }
 
 }
@@ -28,5 +31,34 @@ function getComputerChoice() {
 
 function getPlayerChoice() {
     return prompt('Please make your choice', 'rock,scissors,paper');
+}
+
+function game() {
+    console.log('------------------------------------');
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = getPlayerChoice();
+        let result = playRound(computerSelection,playerSelection);
+        if (result == 'player') {
+            playerScore++;
+        } else if (result == 'tie'){
+        }
+         else {
+            computerScore++;
+        }
+        console.log('Player: ' + playerScore);
+        console.log('Computer: ' + computerScore);
+        console.log('------------------------------------');
+    }
+
+    if (playerScore == computerScore) {
+        console.log('Tied! No Winner');
+    } else if (playerScore > computerScore) {
+        console.log ('Congratulations! You Have Won!');
+    } else {
+        console.log('Try Again! Nice Try!');
+    }
 }
 
